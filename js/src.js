a1e83c03506ff6bb5a31596d2dfede99208b9903
Rob <robwalker@outlook.com>
2024-06-18 21:26:19 -0700

    change the background color

--- js/src.js
+++ js/src.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
 	$("#btn-rawtext, .menu-link").hover(function() {
-		$(this).css("background-color", "#FFF");
-		$(this).css("color", "#7e0000");
-	},function() {
 		$(this).css("background-color", "#7e0000");
-		$(this).css("color", "#fff");
+		$(this).css("color", "#000");
+	},function() {
+		$(this).css("background-color", "#000");
+		$(this).css("color", "#7e0000");
 	});
 
 	$("#fldLeft, #fldMiddleT, #fldRight, #fldMiddleB").hover(function() {
-		$(this).css("background-color", "#fff");
-		$(this).css("color", "#7e0000");
-	}, function() {
 		$(this).css("background-color", "#7e0000");
-		$(this).css("color", "#fff");
+		$(this).css("color", "#000");
+	}, function() {
+		$(this).css("background-color", "#000");
+		$(this).css("color", "#7e0000");
 	});
 
 	$("#menu-restart").click(function() {
