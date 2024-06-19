dcab496d0b0be0cf9021fe9aa7ffe5a6260d49f5
Rob <robwalker@outlook.com>
2024-06-18 21:18:21 -0700

    fix hover color

--- js/src.js
+++ js/src.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 
 	$("#btn-rawtext, .menu-link").hover(function() {
 		$(this).css("background-color", "#FFF");
-		$(this).css("color", "#000");
+		$(this).css("color", "#7e0000");
 	},function() {
-		$(this).css("background-color", "#000");
+		$(this).css("background-color", "#7e0000");
 		$(this).css("color", "#fff");
 	});
 
 	$("#fldLeft, #fldMiddleT, #fldRight, #fldMiddleB").hover(function() {
 		$(this).css("background-color", "#fff");
-		$(this).css("color", "#000");
+		$(this).css("color", "#7e0000");
 	}, function() {
-		$(this).css("background-color", "#000");
+		$(this).css("background-color", "#7e0000");
 		$(this).css("color", "#fff");
 	});
 
